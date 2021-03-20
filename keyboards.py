f6bbe29f026de50198bd3f6e1c2e55e22adadea4
MasterYoooda <54315144+MasterYoooda@users.noreply.github.com>
2021-03-20 21:24:30 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -6,16 +6,22 @@ start_keyboard.add(
     InlineKeyboardButton("Игра с компьютером",callback_data='mode_single'),
     InlineKeyboardButton("Игра с другим игроком",callback_data='mode_multi')
 )
+
+priority_keyboard = InlineKeyboardMarkup()
+priority_keyboard.add(
+    InlineKeyboardButton('"Крестики"',callback_data='cross'),
+    InlineKeyboardButton('"Нолики"',callback_data='zero')
+)
+
 game_keyboard = InlineKeyboardMarkup()
 game_keyboard.add(
-    InlineKeyboardButton('0',callback_data='zero'),
-    InlineKeyboardButton('1',callback_data='one'),
-    InlineKeyboardButton('2',callback_data='two'),
-    InlineKeyboardButton('3',callback_data='three'),
-    InlineKeyboardButton('4',callback_data='four'),
-    InlineKeyboardButton('5',callback_data='five'),
-    InlineKeyboardButton('6',callback_data='six'),
-    InlineKeyboardButton('7',callback_data='seven'),
-    InlineKeyboardButton('8',callback_data='eight'),
-    InlineKeyboardButton('9',callback_data='nine')
+    InlineKeyboardButton('❌',callback_data='one'),
+    InlineKeyboardButton('❌',callback_data='two'),
+    InlineKeyboardButton('❌',callback_data='three'),
+    InlineKeyboardButton('❌',callback_data='four'),
+    InlineKeyboardButton('❌',callback_data='five'),
+    InlineKeyboardButton('❌',callback_data='six'),
+    InlineKeyboardButton('❌',callback_data='seven'),
+    InlineKeyboardButton('❌',callback_data='eight'),
+    InlineKeyboardButton('❌',callback_data='nine')
 )
