1ad1eac9f8e35c52032a620cc154c928744a96cd
negvozd <gvozdkov.na@phystech.edu>
2021-04-04 18:38:39 +0300

    Files added

--- bot.py
+++ bot.py
@@ -1,23 +1,10 @@
 import telebot
-import gametest
-
+import keyboards
+import randomnumbers
+import testimages
+from telebot.types import Message
 
 bot = telebot.TeleBot("1632459413:AAGpEtpehyUJoEvHWlOE6FHWLjl1Mj3_uq8")
-user_Id = 0
-isGameStart = False
-
-
-@bot.message_handler(content_types=['text'])
-def GetTextMessages(message, event = None):
-    if (event == 'cell_number'):
-        bot.register_next_step_handler(message, lambda message: GetPlayerTurn(message))
-
-    if message.text == "sq" and not isGameStart:        
-        global user_Id
-        user_Id = message.from_user.id
-        SendMessage("Сообщение получено")
-        gametest.GameManager()
-
 
 @bot.message_handler(commands=['start'])
 def StartCommand(message):
@@ -25,24 +12,107 @@ def StartCommand(message):
         message.chat.id,
         'Привет!\n'+
         'В будущем тут что-то появится\n'+ '\n'
-        'А может и нет 😈\n'
+        'А может и нет 😈\n' + '\n'
+        'Нажми "/newgame" и не еби себе мозг'
     )
 
+@bot.message_handler(commands=['newgame'])
+def Newgame(message):
+    bot.send_message(
+        message.chat.id,
+        'Выберете режим игры:  ',
+        reply_markup=keyboards.start_keyboard
+    )
 
-def GetPlayerTurn(message):
-    player_input = -1
-    while player_input == -1:
-        if (message.text.isdigit() and int(message.text) < 10 and int(message.text) > 0):
-            player_input = int(message.text)
-        else:
-            SendMessage("Не число")
-            GetTextMessages(message, 'cell_number')
-    return player_input
+    
 
-
-def SendMessage(text):
-    bot.send_message(user_Id, text)
-
+@bot.callback_query_handler(func=lambda c:True)
+def inline(c):
+    if c.data == 'mode_single':
+        bot.send_message(
+            c.message.chat.id,
+            'Вы хотите ходить первым или вторым? Если хотите ходить первым - выберите крестики, вторым - нолики',
+            reply_markup=keyboards.priority_keyboard
+        )
+    if c.data == 'cross':
+        bot.send_photo(
+            c.message.chat.id,
+            photo = open('pol.jpg', 'rb'),
+            caption = 'Выберете на клавиатуре, в какую клетку на поле поставить крестик',
+            reply_markup=keyboards.game_keyboard
+        )
+    if c.data == 'one':
+        testimages.cross(110,110)
+        photo = open('pol2.jpg', 'rb')
+        bot.edit_message_media(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            media=telebot.types.InputMediaPhoto(photo)
+        )
+    if c.data == 'two':
+        testimages.cross(320,110)
+        photo = open('pol2.jpg', 'rb')
+        bot.edit_message_media(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            media=telebot.types.InputMediaPhoto(photo)
+        )
+    if c.data == 'three':
+        testimages.cross(530,110)
+        photo = open('pol2.jpg', 'rb')
+        bot.edit_message_media(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            media=telebot.types.InputMediaPhoto(photo)
+        )
+    if c.data == 'four':
+        testimages.cross(110,320)
+        photo = open('pol2.jpg', 'rb')
+        bot.edit_message_media(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            media=telebot.types.InputMediaPhoto(photo)
+        )
+    if c.data == 'five':
+        testimages.cross(320,320)
+        photo = open('pol2.jpg', 'rb')
+        bot.edit_message_media(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            media=telebot.types.InputMediaPhoto(photo)
+        )
+    if c.data == 'six':
+        testimages.cross(530,320)
+        photo = open('pol2.jpg', 'rb')
+        bot.edit_message_media(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            media=telebot.types.InputMediaPhoto(photo)
+        )
+    if c.data == 'seven':
+        testimages.cross(110,530)
+        photo = open('pol2.jpg', 'rb')
+        bot.edit_message_media(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            media=telebot.types.InputMediaPhoto(photo)
+        )
+    if c.data == 'eight':
+        testimages.cross(320,530)
+        photo = open('pol2.jpg', 'rb')
+        bot.edit_message_media(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            media=telebot.types.InputMediaPhoto(photo)
+        )
+    if c.data == 'nine':
+        testimages.cross(530,530)
+        photo = open('pol2.jpg', 'rb')
+        bot.edit_message_media(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            media=telebot.types.InputMediaPhoto(photo)
+        )
 
 if __name__ == '__main__':
     bot.infinity_polling()
